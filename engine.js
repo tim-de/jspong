c7d17d82a38e3afcdd539810449beeed3ac28fd4
Tim Evans <evanstde@gmail.com>
2023-05-16 23:49:43 +0100

    Add shared behaviours to Actor class
    Moves functionality required in any game object into the
    Actor class, such as updating position

--- engine.js
+++ engine.js
@@ -26,15 +26,6 @@
  * @property {number} y
  */
 
-/**
- * @typedef {object} Actor
- * @property {Bounds} bounds
- * @property {Vector2} pos
- * @property {Vector2} vel
- * @property {FillStyle} fillStyle
- * @property {CanvasRenderingContext2D} context
- */
-
 /** A class representing a generic actor in a scene */
 class Actor {
     /**
@@ -42,14 +33,74 @@ class Actor {
      * @param {Bounds} bounds - The area the actor is limited to
      * @param {Vector2} initPos - The starting position
      * @param {Vector2} initVel - The starting velocity
+     * @param {Vector2} size - The width and height of the Actor
      * @param {FillStyle} fillStyle - The fill style to be passed on to the context when drawing the actor
      * @param {CanvasRenderingContext2D} context - The 2d canvas context the actor is to be drawn into
      */
-    constructor(bounds, initPos, initVel, fillStyle, context) {
+    constructor(bounds, initPos, initVel, size, fillStyle, context) {
+        /** @type {Bounds} */
         this.bounds = bounds;
+        /** @type {Vector2} */
         this.pos = initPos;
+        /** @type {Vector2} */
         this.vel = initVel;
+        /** @type {Vector2} */
+        this.size = size;
+        /** @type {FillStyle} */
         this.fillStyle = fillStyle;
+        /** @type {CanvasRenderingContext2D} */
         this.context = context;
+    }
+
+    /**
+     * Draw the Actor as a rectangle in the specified context
+     */
+    draw() {
+        this.context.beginPath();
+        this.context.rect(this.pos.x - (this.size.x / 2), this.pos.y - (this.size.y / 2), this.size.x, this.size.y);
+        this.context.fillStyle = this.fillStyle;
+        this.context.fill();
+        this.context.closePath();
+    }
+
+    /**
+     * Ensure the Actor is within the specified bounds
+     */
+    boundsClamp() {
+        if (this.pos.x - this.bounds.minX < this.size.x / 2) {
+            this.pos.x = this.bounds.minX + this.size.x / 2;
+        }
+        else if (this.bounds.maxX - this.pos.x < this.size.x / 2) {
+            this.pos.x = this.bounds.maxX - this.size.x / 2;
+        }
+
+        if (this.pos.y - this.bounds.minY < this.size.y / 2) {
+            this.pos.y = this.bounds.minY + this.size.y / 2;
+        }
+        else if (this.bounds.maxY - this.pos.y < this.size.y / 2) {
+            this.pos.y = this.bounds.maxY - this.size.y / 2;
+        }
+    }
+
+    /**
+     * Handle any collisions with the edges of the bound areas
+     */
+    bounceCheck() {
+        if (this.pos.x - this.bounds.minX <= this.size.x / 2 || this.bounds.maxX - this.pos.x <= this.size.x / 2) {
+            this.vel.x = -this.vel.x;
+        }
+        if (this.pos.y - this.bounds.minY <= this.size.y / 2 || this.bounds.maxY - this.pos.y <= this.size.y / 2) {
+            this.vel.y = -this.vel.y;
+        }
+    }
+
+    /**
+     * Update the position of the Actor
+     */
+    updatePos() {
+        this.boundsClamp();
+        this.bounceCheck();
+        this.pos.x += this.vel.x;
+        this.pos.y += this.vel.y;
     }
 }
